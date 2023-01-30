0c927e674c8cb84e3f68731f82570ad49c74258d
haroonniaz96github <haroonniaz96github@gmail.com>
2023-01-30 20:07:48 +0500

    authentication issue is resolved

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Store;
 use App\Models\Category;
 use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Tag;
 use Slince\Shopify\Client;
 use Slince\Shopify\Auth\OAuth;
